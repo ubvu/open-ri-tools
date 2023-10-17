c25fab43a1cac9c18cb62f11ef57bd4c31ff87dd
Max Paulus <paulmaxus@gmail.com>
2023-10-17 16:28:59 +0200

    added heroku files

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+while read line; do 
+	appname=$(echo $line | cut -f 2 -d ',')
+	appcode=$(echo $line | cut -f 3 -d ',')
+	appurl=$(echo "${appname}-${appcode}.herokuapp.com")
+	# add necessary buildpacks
+ 	heroku buildpacks:set heroku/python -a $appname
+ 	heroku buildpacks:add -a $appname heroku-community/multi-procfile
+	# reference procfile location
+ 	heroku config:set -a $appname PROCFILE=procfiles/$appname-${appcode}/Procfile
+	# push to heroku
+	giturl=$(echo "https://git.heroku.com/${appname}.git") 
+	git push $giturl heroku:main
+done
