5217c3857361a2bb27fc5535e08716ec9d9f5e4f
paulmaxus <paulmaxus@users.noreply.github.com>
2023-10-18 07:58:12 +0000

    Update heroku to output generated at 397f007

--- publish.sh
+++ publish.sh
@@ -11,9 +11,7 @@ while read line; do
  	heroku buildpacks:add -a $appname heroku-community/multi-procfile
 	# reference procfile location
  	heroku config:set -a $appname PROCFILE=procfiles/$appname-${appcode}/Procfile
-	# reset repo
- 	heroku repo:reset -a $appname
- 	# push to heroku
+	# push to heroku
 	giturl=$(echo "https://git.heroku.com/${appname}.git") 
 	git push $giturl heroku:main
 done < deployments
